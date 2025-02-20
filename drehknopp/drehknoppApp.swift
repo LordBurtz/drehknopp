72e03f1c07468ed18648ef61df1ec57cb60359be
fridolin.karger <fridolin.karger@gmail.com>
2025-02-20 12:47:16 +0100

    Initial Commit

---
+++ drehknopp/drehknoppApp.swift
@@ -0,0 +1,17 @@
+//
+//  drehknoppApp.swift
+//  drehknopp
+//
+//  Created by Fridolin Karger on 20.02.25.
+//
+
+import SwiftUI
+
+@main
+struct drehknoppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ drehknoppTests/drehknoppTests.swift
@@ -0,0 +1,17 @@
+//
+//  drehknoppTests.swift
+//  drehknoppTests
+//
+//  Created by Fridolin Karger on 20.02.25.
+//
+
+import Testing
+@testable import drehknopp
+
+struct drehknoppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
